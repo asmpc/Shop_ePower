7819863790ec3fe3a6fe8014fecc67fa27dadacc
Alix <asmpc@mail.ru>
2026-07-24 14:23:12 +0300

    17.2 - mplement async order notifications with elery tasks

---
+++ src/shop_epower/notifications/tasks.py
@@ -0,0 +1,45 @@
+from celery import shared_task
+
+from shop_epower.orders.models import Order
+from shop_epower.notifications.services import (
+    send_order_created_email_to_customer,
+    send_order_created_notification_to_managers,
+)
+
+
+@shared_task(
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={
+        "max_retries": 3,
+    },
+    acks_late=True,
+)
+def send_customer_order_created_email(order_id):
+
+    order = Order.objects.get(
+        pk=order_id,
+    )
+
+    send_order_created_email_to_customer(
+        order,
+    )
+
+
+@shared_task(
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={
+        "max_retries": 3,
+    },
+    acks_late=True,
+)
+def notify_managers_about_new_order(order_id):
+
+    order = Order.objects.get(
+        pk=order_id,
+    )
+
+    send_order_created_notification_to_managers(
+        order,
+    )
